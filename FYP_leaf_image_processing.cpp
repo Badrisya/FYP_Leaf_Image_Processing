864c9632b6f3e18a5e87f0a0f017b45d365958d1
Badrisya <mbadrisya@gmail.com>
2022-03-03 00:50:59 +0800

    Add project files.

---
+++ FYP_leaf_image_processing.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	string image_path = "/Users/user/OneDrive - Universiti Teknologi PETRONAS/Documents/UTP/4th2nd/FYP/Site/visit 1/idk/IMG_1491.JPG";
+
+	Mat image = imread(image_path, IMREAD_COLOR);
+	resize(image, image, { 1000,700 }, 0, 0, cv::INTER_NEAREST);
+
+	imshow("Image", image);
+
+	waitKey(0);
+
+	return 0;
+}
